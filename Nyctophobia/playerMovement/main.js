7b1900de0ae1b5bb34c83431685610fabf7e0988
bsmithfa <bsmithfa@fitc-consult-mac01.lt.ucsc.edu>
2017-08-02 12:19:50 -0700

    added walls, capped player movement speed in any direction

--- Nyctophobia/playerMovement/main.js
+++ Nyctophobia/playerMovement/main.js
@@ -1,16 +1,27 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
-var ship;
+var player;
 
 function preload() {
-	game.load.spritesheet('ship', 'Assets/fullsheet.png', 16, 16);
+	game.load.image('player', 'Assets/triangle.jpg');
+	game.load.image('wall', 'Assets/purplebrick.png');
 }
 
 function create() {
 	game.physics.startSystem(Phaser.Physics.ARCADE);
-//	cursors = game.input.keyboard.addKey(Phaser.Keyboard.R);
-
-	ship = new Player(game, Math.random() * 800, Math.random() * 600, 'ship', Math.floor(Math.random() * 18));
-	game.add.existing(ship);
+	
+	walls = game.add.group();
+	for(var x = 0; x < 8; x++){
+		wall = new Wall(game, 100 + (x * 70), 100, 'wall');
+		walls.add(wall);
+	}
+	for(var y = 0; y < 8; y++){
+		wall = new Wall(game, 100, 100 + (y * 50), 'wall');
+		walls.add(wall);
+	}
+	
+	player = new Player(game, 400, 400, 'player', null, walls);
+	player.scale.setTo(.3);
+	game.add.existing(player);
 }
 
 function update() {
--- Nyctophobia/playerMovement/player.js
+++ Nyctophobia/playerMovement/player.js
@@ -2,8 +2,9 @@ var w;
 var a;
 var s;
 var d;
+var vtotal;
 
-function Player(game, x, y, key, frame) {
+function Player(game, x, y, key, frame, walls) {
 	Phaser.Sprite.call(this, game, x, y, key, frame);
 	game.physics.enable(this);
 	this.enableBody = true;
@@ -19,9 +20,10 @@ Player.prototype = Object.create(Phaser.Sprite.prototype);
 Player.prototype.constructor = Player;
 
 Player.prototype.update = function() {
-//	game.physics.arcade.collide(this, walls);
-	this.rotation = game.physics.arcade.angleToPointer(this);
-
+	game.physics.arcade.collide(this, walls);
+	this.rotation = game.physics.arcade.angleToPointer(this) + (Math.PI / 2);
+	
+	// up/down movement
 	if(w.isDown && this.body.velocity.y > -100){
 		this.body.velocity.y -= 10;
 	}
@@ -34,7 +36,8 @@ Player.prototype.update = function() {
 	else if (this.body.velocity.y > 0){
 		this.body.velocity.y -= 5;
 	}
-
+	
+	// left/right movement
 	if(a.isDown && this.body.velocity.x > -60){
 		this.body.velocity.x -= 10;
 	}
@@ -46,5 +49,17 @@ Player.prototype.update = function() {
 	}
 	else if (this.body.velocity.x > 0){
 		this.body.velocity.x -= 5;
+	}
+	
+	//total speed capped at 100
+	vtotal = Math.abs(this.body.velocity.x) + Math.abs(this.body.velocity.y);
+	while(vtotal > 100){
+		if(Math.abs(this.body.velocity.x) > Math.abs(this.body.velocity.y)){
+			this.body.velocity.x > 0 ? this.body.velocity.x-- : this.body.velocity.x++;
+		}
+		else{
+			this.body.velocity.y > 0 ? this.body.velocity.y-- : this.body.velocity.y++;
+		}
+		vtotal--;
 	}
 }
