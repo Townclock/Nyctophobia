403966a358f79444a0fa80836b8f42e60dd302fa
Jay Parikh <jrparikh@ucsc.edu>
2017-08-03 10:14:06 -0700

    enemy collides with player

--- Nyctophobia/enemyTesting/enemy.js
+++ Nyctophobia/enemyTesting/enemy.js
@@ -1,16 +1,16 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
-	game.load.image('star', 'assets/img/star.png');
-    game.load.image('diamond', 'assets/img/diamond.png');
+	game.load.image('monster', 'assets/img/star.png');
+    game.load.image('player', 'assets/img/diamond.png');
 }
 
 function create() {
-	diamond = game.add.sprite(100, 100, 'diamond');
-	star = game.add.sprite(200, 100, 'star');
+	player = game.add.sprite(100, 100, 'player');
+	monster = game.add.sprite(200, 100, 'monster');
 
-	game.physics.arcade.enable(diamond);
-    game.physics.arcade.enable(star);
+	game.physics.arcade.enable(player);
+    game.physics.arcade.enable(monster);
 
 
 }
@@ -18,33 +18,40 @@ function create() {
 function update() {
 	cursors = game.input.keyboard.createCursorKeys();
 	
-	diamond.body.velocity.x = 0;
-	diamond.body.velocity.y = 0;
+	player.body.velocity.x = 0;
+	player.body.velocity.y = 0;
 
 	//star.body.velocity = 100;
 	//Diamond movement
     if (cursors.left.isDown)
     {
         //Move to the left
-        diamond.body.velocity.x = -150;
+        player.body.velocity.x = -150;
     }
     else if (cursors.right.isDown)
     {
         //Move to the right
-        diamond.body.velocity.x = 150;
+        player.body.velocity.x = 150;
     }else if (cursors.up.isDown)
     {
         //Move to the right
-        diamond.body.velocity.y = -150;
+        player.body.velocity.y = -150;
     }else if (cursors.down.isDown)
     {
         //Move to the right
-        diamond.body.velocity.y = 150;
+        player.body.velocity.y = 150;
     }
 
     //find angle for monster and character
-    var targetAngle = game.physics.arcade.angleBetween(diamond, star);
+    var targetAngle = game.physics.arcade.angleBetween(player, monster);
 
-    star.body.velocity.x = -50 * Math.cos(targetAngle);
-    star.body.velocity.y = -50 * Math.sin(targetAngle);
+    monster.body.velocity.x = -50 * Math.cos(targetAngle);
+    monster.body.velocity.y = -50 * Math.sin(targetAngle);
+
+    function destroyPlayer(player, monster){
+        player.kill();
+    }
+
+    game.physics.arcade.overlap(player, monster, destroyPlayer, null, this);
+
 }
