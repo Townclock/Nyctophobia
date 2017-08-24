46a75ccb88ef4751b5c20c011ad01f4111338afb
Jay Parikh <jrparikh@ucsc.edu>
2017-08-24 03:18:37 +0100

    Monster now moves to the closer torch

--- js/prefabs/enemy.js
+++ js/prefabs/enemy.js
@@ -11,9 +11,21 @@ Monster.prototype.constructor = Monster;
 
 Monster.prototype.update = function() {
 
-    //enemy moves towards player
-    game.physics.arcade.moveToObject(this, player, 100);
-    this.rotation = game.physics.arcade.angleBetween(this, player) + (Math.PI / 2);
+    for(i = 0; i <= lights.length; i++){
+        Phaser.Math.distance(lights.getAt(i).x, lights.getAt(i).y, this.x, this.y);
+        //console.log('Distance of '+ i + ' is ' + Phaser.Math.distance(lights.getAt(i).x, lights.getAt(i).y, this.x, this.y));
+    }
+
+     if(Phaser.Math.distance(lights.getChildAt(0).x, lights.getChildAt(0).y, this.x, this.y) < 
+            Phaser.Math.distance(lights.getChildAt(1).x, lights.getChildAt(1).y, this.x, this.y)){
+                game.physics.arcade.moveToObject(this, lights.getChildAt(0), 100);
+                this.rotation = game.physics.arcade.angleBetween(this, lights.getChildAt(0)) + (Math.PI / 2);
+                //console.log('moving to 0') 
+    }else{
+                game.physics.arcade.moveToObject(this, lights.getAt(1), 100);
+                this.rotation = game.physics.arcade.angleBetween(this, lights.getChildAt(1)) + (Math.PI / 2);
+                //console.log('moving to 1');
+    }
 
     function destroyPlayer(player, monster) {
 
