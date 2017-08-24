2ede46bdad21026aed77d6c344cc37ad1250543f
Buzz Tilford <tilford20@gmail.com>
2017-08-24 10:37:42 -0700

    fixed image loading

--- js/states/load.js
+++ js/states/load.js
@@ -7,10 +7,9 @@ Load.prototype = {
         game.load.image('title', 'Title.png');
         game.load.spritesheet('player', 'walking1.png', 80, 115, 6);
         game.load.image('wall', 'purplebrick.png');
-        //game.load.image('monster', 'triangle2.png');
-        game.load.image('circle', 'bitmapCircle.png');
-        game.load.image('torch', 'New size images/torch(s).png');
-        game.load.image('battery', 'New size images/battery(s).png');
+        game.load.image('monster', 'monster.png');
+        game.load.image('torch', 'torch.png');
+        game.load.image('battery', 'battery.png');
         game.load.image('background', 'background.png');
     },
 
